7619e52015ad886f9d6fb668b5e630b9a9103bff
ayush-pal02 <ayushpal8ps@gmail.com>
2024-10-09 13:10:06 +0530

    loginform

---
+++ day5/script.js
@@ -0,0 +1,28 @@
+"use strict";
+// let a = 22/7;
+// let b ="hello";
+// let c =[1,2,3,4]
+// let e={
+//     name:"arun",
+//     id:32
+// }
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof e)
+
+// function display(){
+//     let x = promt("enter your name" ,"Ayush");
+//     alert(x);
+// }
+function validate(){
+    let login=document.getElementById("login").value;
+    let pass=document.getElementById("pass").value;
+    if(login=="admin" && pass=="admin"){
+        p1.innerText="login success"
+    }
+    else p1.innerText ="Login Failed";
+    return false;
+
+}
+
